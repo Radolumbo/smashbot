4a3360f460f787d257d722d4d0bbaf57c31c0954
zeroviim <43391837+zeroviim@users.noreply.github.com>
2019-04-17 08:24:59 -0400

    Add files via upload
    SQL sanitization and formatting within code

--- sb_messaging_utils.py
+++ sb_messaging_utils.py
@@ -3,14 +3,21 @@ from sb_constants import embed_color
 
 async def send_profile(channel, db, user):
     cursor = db.cursor()
-    cursor.execute('''
+    query= '''
         SELECT
             switch_tag, switch_code
-        FROM player p  
-        INNER JOIN guild_member g
-            ON p.discord_id = g.player_discord_id
-        WHERE g.guild_id = {} AND p.discord_id = {}'''.format(channel.guild.id, user.id))
+        FROM 
+            player p  
+        INNER JOIN 
+            guild_member g
+        ON 
+            p.discord_id = g.player_discord_id
+        WHERE 
+            g.guild_id = %s 
+        AND 
+            p.discord_id = %s'''
 
+    cursor.execute(query, (channel.guild.id, user.id))
     row = cursor.fetchone()
         
     embed = discord.Embed(color=embed_color)
